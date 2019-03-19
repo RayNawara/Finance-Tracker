35c0e59005495aa839367e4fae235b54b89f9113
Ray Nawara <rjnawara@gmail.com>
2019-03-19 10:53:47 -0400

    Add Stock search to my_portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  root 'welcome#index'
+  root 'users#my_portfolio'
+  get 'my_portfolio', to: "users#my_portfolio"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
